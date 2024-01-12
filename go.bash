912b75e945cd0c762a92b8eb5572ab612ce16d24
Sublight711 <105186638+Sublight711@users.noreply.github.com>
2024-01-12 16:05:23 -0600

    Update go.bash

--- go.bash
+++ go.bash
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Starting custom Linux setup script by Travis M.  This will update your system, and add useful programs to your Debian (Ubuntu, Kali, etc) Linux Distributions.  Choose yes to all questions!"
+echo "----------------------------------"
 echo "alias dir='ls -alF'" >> .bash_aliases
+echo "alias dir='ls -alF'" >> sudo apt-
 echo "Updating Repositories"
 sudo apt-get update
 echo "Updating OS - Choose yes to questions"
