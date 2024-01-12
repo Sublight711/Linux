da6321fbb631466c3c2e15af05cffc8d66814e5a
Sublight711 <105186638+Sublight711@users.noreply.github.com>
2024-01-12 16:13:33 -0600

    Update go.bash

--- go.bash
+++ go.bash
@@ -2,7 +2,6 @@
 echo "Starting custom Linux setup script by Travis M.  This will update your system, and add useful programs to your Debian (Ubuntu, Kali, etc) Linux Distributions.  Choose yes to all questions!"
 echo "----------------------------------"
 echo "alias dir='ls -alF'" >> .bash_aliases
-echo "alias dir='ls -alF'" >> sudo apt-
 echo "Updating Repositories"
 sudo apt-get update
 echo "Updating OS - Choose yes to questions"
@@ -10,3 +9,6 @@ sudo apt-get upgrade
 echo "Installing programs"
 sudo apt-get install gedit
 sudo apt-get install fish
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo apt-get install -f
