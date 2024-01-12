e2a19ed67f75a8bd73e4994bcd1ae6ff71b5b8a5
Sublight711 <105186638+Sublight711@users.noreply.github.com>
2024-01-12 23:18:12 +0000

    Update go.sh

--- go.sh
+++ go.sh
@@ -7,12 +7,20 @@ echo "alias update='./update.sh'" >> ~/.bash_aliases
 # Ensure update.sh is executable
 chmod +x ./update.sh
 
+echo "Adding Fish shell PPA and installing Fish"
+sudo add-apt-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+
+# Set Fish as the default shell
+chsh -s /usr/bin/fish
+
 echo "Updating Repositories"
 sudo apt-get update
 echo "Updating OS - Choose yes to questions"
 sudo apt-get upgrade -y
 echo "Installing programs"
-sudo apt-get install -y gedit fish
+sudo apt-get install -y gedit
 
 # Download Chrome only if it's not already installed
 if ! dpkg -l | grep -q google-chrome-stable; then
