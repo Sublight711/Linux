dbafc0188d0b4e3d8710d71348190d08d1786f5f
Sublight711 <105186638+Sublight711@users.noreply.github.com>
2024-01-12 16:01:51 -0600

    Update go.bash

--- go.bash
+++ go.bash
@@ -1,1 +1,10 @@
-echo "Ok first step is done"
+#!/bin/bash
+echo "Starting custom Linux setup script by Travis M.  This will update your system, and add useful programs to your Debian (Ubuntu, Kali, etc) Linux Distributions.  Choose yes to all questions!"
+echo "alias dir='ls -alF'" >> .bash_aliases
+echo "Updating Repositories"
+sudo apt-get update
+echo "Updating OS - Choose yes to questions"
+sudo apt-get upgrade
+echo "Installing programs"
+sudo apt-get install gedit
+sudo apt-get install fish
