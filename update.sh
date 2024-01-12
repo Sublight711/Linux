77319a314c2b622d908e86af5f60fe0cdf77f664
Sublight711 <105186638+Sublight711@users.noreply.github.com>
2024-01-12 16:55:16 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,6 +16,10 @@ git pull origin
 
 echo "Updating Repositories"
 sudo apt-get update
-echo "Updating OS"
+echo "Updating OS - Choose yes to questions"
 sudo apt-get upgrade -y
+
+# Update Firefox
+sudo apt-get install --only-upgrade firefox -y
+
 sudo apt upgrade -y google-chrome-stable
